cbe3595126cc68fbafb43a5f79258fa6084b2e76
hoonha choi <sosqsz2@naver.com>
2020-12-12 16:58:27 +0900

    ♻️ 기능 변경 : 입력 화면 변경

--- codesquad/main.swift
+++ codesquad/main.swift
@@ -9,24 +9,11 @@ import Foundation
 
 
 while true {
+    print("1단계: 단어 밀어내기")
+    let input = readLine()!.components(separatedBy: " ")
+    var pushword = PushWord(word: input[0],
+                            count: Int(input[1]) ?? 0 ,
+                            control: Control(rawValue: input[2]))
+    pushword.process()
     print()
-    print("=============================")
-    print("선택해주세요")
-    print("1. 단어 밀어내기")
-    print("=============================")
-    print()
-    switch Int(readLine()!) ?? 0 {
-    case 1:
-        print("1단계: 단어 밀어내기")
-        let input = readLine()!.components(separatedBy: " ")
-        var pushword = PushWord(word: input[0],
-                                count: Int(input[1]) ?? 0 ,
-                                control: Control(rawValue: input[2]))
-        pushword.process()
-    case 9 :
-        print("종료")
-        exit(0)
-    default:
-        print("숫자만 입력해주세요")
-    }
 }
